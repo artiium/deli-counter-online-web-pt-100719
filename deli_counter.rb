8b2de89628e3ba121c08b4582e300c30188321e0
Artiium Arbyummi <artiium21@gmail.com>
2019-10-16 03:04:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(array)
     puts "The line is currently empty."
  else 
    array.each_with_index(1) { |person, index| puts "The line is currently: #{index}. #{person}"}
-end
+ end
 
 end
  
