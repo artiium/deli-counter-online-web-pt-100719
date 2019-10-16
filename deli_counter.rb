37cd8345e2d20152fa22cf7fefd53de3d826eb2c
Artiium Arbyummi <artiium21@gmail.com>
2019-10-16 03:04:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 def line(array)
   if array.empty?
     puts "The line is currently empty."
- else array.each_with_index(1) { |person, index| puts "The line is currently: #{index}. #{person}"}
+ else 
+   array.each_with_index(1) { |person, index| puts "The line is currently: #{index}. #{person}"}
 end
 
 end
